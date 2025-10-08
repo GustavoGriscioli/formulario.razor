79dbb5a58ddccf1a0901bed61c5d9cac0c00ae41
GUSTAVOMARCELOGRISCI <GUSTAVOMARCELOGRISCI@WIN-1TV51GN5UFT>
2025-10-08 08:36:24 -0300

    V2.0

---
+++ MauiApp1/Components/DAO/ClienteDAO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Components.DAO
+{
+ public class ClienteDAO 
+    {
+        public async Task<bool>SalvarCliente(ClienteDAO novoCliente)
+        {
+            try
+            {
+                //string de conexão com o banco de dados MySQL
+                string connectionString = "server=localhost;user=root;password=root;database=gatinhos";
+                
+                await using var conn = new MySqlConnector(connectionString)
+            }
+
+            catch
+            {
+
+            }
+        }
+    }
+}
