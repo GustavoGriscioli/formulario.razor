e4c41d2fe7862b63d2984a978148c32176c76a25
GUSTAVOMARCELOGRISCI <GUSTAVOMARCELOGRISCI@WIN-1TV51GN5UFT>
2025-10-09 08:26:14 -0300

    Enviando dados para a tabela MySql

--- MauiApp1/Components/DAO/ClienteDAO.cs
+++ MauiApp1/Components/DAO/ClienteDAO.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySqlConnector;
+using MauiApp1.Components.model;
 
 namespace MauiApp1.Components.DAO
 {
  public class ClienteDAO 
     {
-        public async Task<bool>SalvarCliente(ClienteDAO novoCliente)
+        public async Task<bool>SalvarCliente(Cliente novoCliente)
         {
             try
             {
@@ -21,11 +22,20 @@ namespace MauiApp1.Components.DAO
                 await conn.OpenAsync();
 
                 string sql = "INSERT INTO tb_cliente (nome,cpf,telefone) VALUES(@nome,@telefone,@telefone)";
+
+                await using var cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@nome", novoCliente.nome);
+                cmd.Parameters.AddWithValue("@cpf", novoCliente.cpf);
+                cmd.Parameters.AddWithValue("@telefone", novoCliente.telefone);
+
+                int rows = await cmd.ExecuteNonQueryAsync();
+
+                return rows > 0;
             }
 
             catch (Exception ex)
             {
-
+                return false;
             }
         }
     }
